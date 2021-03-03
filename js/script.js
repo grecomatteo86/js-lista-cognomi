d1d4b21182dd4d6811a649321deb5e77662078e8
Matteo <grecomatteo86@gmail.com>
2021-03-03 15:47:19 +0100

    fixes

--- js/script.js
+++ js/script.js
@@ -5,18 +5,17 @@ stampa la lista ordinata alfabeticamente
 scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 */
 
-
-//1. chiedo all'utente il cognome
-var cognomeUtente = prompt('scrivi il tuo cognome');
-console.log(cognomeUtente);
-
-//2. creo un array con altri cognomi
+//1. creo un array con dei cognomi
 var listaCognomi = ['Bianchi','Rossi','Duzioni','Balsamo','Verdi'];
-console.log(listaCognomi);
+console.log('l\'array originario è ' + listaCognomi);
+
+//2. chiedo all'utente il suo cognome
+var cognomeUtente = prompt('scrivi il tuo cognome');
+console.log('il cognome inserito dall\'utente è ' + cognomeUtente);
 
 //3. inserisco il cognome dell'utente nell'array
 listaCognomi.push(cognomeUtente);
-console.log(listaCognomi);
+console.log('l\'array con inserito il cognome dell\'utente è ' + listaCognomi);
 
 //4. stampo la lista ordinata alfabeticamente
-console.log(listaCognomi.sort());
+console.log('l\'array ordinato alfabeticamente è ' + listaCognomi.sort());

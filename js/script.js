8bb3909132f9f9ee2aaa86a21c288e9a16dc2ea2
Matteo <grecomatteo86@gmail.com>
2021-03-03 15:24:36 +0100

    chiedo all'utente il cognome

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Chiedere all’utente il cognome
+inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+*/
+
+
+//1. chiedo all'utente il cognome
+var cognome = prompt('scrivi il tuo cognome');
+console.log(cognome);
